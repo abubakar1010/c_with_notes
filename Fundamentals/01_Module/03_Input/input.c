7f5a3bb6aa1a1f5decd6521cb700a6436601af52
Abubakar1010 <abubakar850772@gmail.com>
2024-04-01 18:40:57 +0600

    data types in c

---
+++ Fundamentals/01_Module/03_Input/input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+{
+    // int score;
+    // float ant;
+    // char a;
+    // scanf("%d %f %c",&score, &ant, &a);
+
+    // printf("%d %0.4f %c", score, ant, a);
+
+    int a,b;
+    // char p;
+
+    // scanf("%d%c %d%c", &a,&p,&b,&p);
+
+    // printf("%d%% %d%%",a,b);
+    scanf("%d%% %d%%", &a, &b);
+
+    printf("%d%% %d%%", a, b);
+}
---
+++ first.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main()
+
+// {
+//     printf("Hello World");
+// }
+
+#include <stdio.h>
+
+int main()
+
+{
+    printf("hello world");
+
+    return 0;
+}
