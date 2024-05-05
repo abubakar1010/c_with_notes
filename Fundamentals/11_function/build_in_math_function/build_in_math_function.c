d9b0051801630ad08b42ab54f39187059ab708db
Abubakar1010 <abubakar850772@gmail.com>
2024-05-05 11:35:31 +0600

    finished ceil, floor, round, sqrt, pow and abs build in function

---
+++ Fundamentals/11_function/build_in_math_function/build_in_math_function.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+
+{
+
+    //ceil function 
+
+        double x;
+        // scanf("%lf", &x);
+        int resultOfCeil = ceil(x);
+        // printf("%d ", resultOfCeil);
+
+
+
+    //floor function 
+
+        double y;
+        // scanf("%lf", &y);
+        int resultOfFloor = floor(y);
+        // printf("%d ", resultOfFloor);
+        
+
+
+    //round function 
+
+        double z;
+        // scanf("%lf", &z);
+        int resultOfRound = round(z);
+        // printf("%d ", resultOfRound);
+
+
+    //sqrt function 
+
+        int c;
+        // scanf("%d", &c);
+        int resultOfSqrt = sqrt(c);
+        // printf("%d ", resultOfSqrt);
+
+
+    //pow function 
+
+        int a, b;
+        // scanf("%d %d", &a, &b);
+        int resultOfPower = pow(a,b);
+        // printf("%d ", resultOfPower);
+
+
+
+
+    //abs function 
+
+        int e;
+        scanf("%d", &e);
+        int resultOfAbs = abs(e);
+        printf("%d ", resultOfAbs);
+
+    return 0;
+}
