f44d108f62ebde3469d36d11b5971b7e2fa0da15
Abubakar1010 <abubakar850772@gmail.com>
2024-05-05 10:59:48 +0600

    call function without parameter and return value

---
+++ Fundamentals/11_function/return_without_parameter/return_without_parameter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int sum(){
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;
+
+    return sum;
+}
+
+int main()
+
+
+{
+        printf("%d ", sum());
+    return 0;
+}
